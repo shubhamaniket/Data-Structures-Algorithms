404da31056944697c6453e3163c995af3cd2519d
shubhamaniket <shubhamaniket6@gmail.com>
2021-02-01 22:46:37 +0530

    Added Bubble Sort

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    bubbleSort(arr,n);
+    cout << "Sorted List : ";
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
