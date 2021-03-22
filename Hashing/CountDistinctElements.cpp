992f79c2f7b4f4c3685a2b96332955599a76c66f
shubhamaniket <shubhamaniket6@gmail.com>
2021-03-22 23:09:19 +0530

    Added in Hashing

--- Hashing/CountDistinctElements.cpp
+++ Hashing/CountDistinctElements.cpp
@@ -1,3 +1,6 @@
+/*
+	Count of distinct elements
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -5,6 +8,13 @@ int main(){
 	int n;
 	cin >> n;
 	int arr[n];
-	
+	for(int i=0;i<n;i++){
+		cin >> arr[i];
+	}
+	unordered_set<int> s;
+	for(int i=0;i<n;i++){
+		s.insert(arr[i]);
+	}
+	cout << s.size();
 	return 0;
 }
